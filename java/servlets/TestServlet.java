0daf1da8d6192698779ee0b03d9c211610d90e58
wtg1890 <william.gentry02@gmail.com>
2018-01-05 09:33:30 -0500

    updated LoginServlet, about to move all code from EmployeeReimbursementSystem (Dynamic Web App 3.0) to ERS (Maven WebApp)

---
+++ java/servlets/TestServlet.java
@@ -0,0 +1,55 @@
+package com.gtrain.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/html/login")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath()+"\n");
+		PrintWriter pw = response.getWriter();
+		response.setContentType("text/html");
+		pw.println("<html><body>");
+		
+		String username = request.getParameter("loginUsername");
+		String password = request.getParameter("loginPassword");
+		
+		pw.println("<p>Username value input: " + username + "</p>");
+		pw.println("<p>Password value input: " + password + "</p>");
+		
+		pw.println("</body></html>");
+		pw.close();
+	}
+
+}
